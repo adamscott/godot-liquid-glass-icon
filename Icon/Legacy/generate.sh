e7de82305ee69db4f6c302a7e063d6e51b98b56f
Adam Scott <ascott.ca@gmail.com>
2025-07-18 14:53:45 -0400

    Create a single iconset

--- Icon/Legacy/generate.sh
+++ Icon/Legacy/generate.sh
@@ -7,15 +7,16 @@ if ! command -v gm >/dev/null 2>&1; then
     exit 1
 fi
 
-cd $(dirname "$(realpath $0)")
+cd "$(dirname "$(realpath "$0")")"
 
 convert_icon () {
     ICON_TYPE="$1"
     ICON_SRC="$2"
+    ICON_DARK_SUFFIX="$3"
 
-    ICON_SIZE=$(gm identify -format "%w,%h" $ICON_SRC)
+    ICON_SIZE=$(gm identify -format "%w,%h" "$ICON_SRC")
 
-    if [ $ICON_SIZE != "2048,2048" ]; then
+    if [ "$ICON_SIZE" != "2048,2048" ]; then
         echo "Source icon must be of size 2048x2048 (currently \"$ICON_SIZE\")"
         exit 1
     fi
@@ -24,12 +25,12 @@ convert_icon () {
         FORMAT="png"
         QUALITY="90"
         WIDTH="$1"
-        TWOX_WIDTH=$(expr $WIDTH \* 2)
+        TWOX_WIDTH=$(( WIDTH * 2 )) 
 
         # 2x.
-        gm convert $ICON_SRC -resize "$TWOX_WIDTH"x"$TWOX_WIDTH" -format $FORMAT -quality $QUALITY "$ICON_TYPE.iconset"/icon_"$WIDTH"x"$WIDTH"@2x.png
+        gm convert "$ICON_SRC" -resize "${TWOX_WIDTH}x${TWOX_WIDTH}" -format $FORMAT -quality $QUALITY "${ICON_TYPE}.iconset/icon_${WIDTH}x${WIDTH}@2x${ICON_DARK_SUFFIX}.png"
         # 1x.
-        gm convert $ICON_SRC -resize "$WIDTH"x"$WIDTH" -format $FORMAT -quality $QUALITY "$ICON_TYPE.iconset"/icon_"$WIDTH"x"$WIDTH".png
+        gm convert "$ICON_SRC" -resize "${WIDTH}x${WIDTH}" -format $FORMAT -quality $QUALITY "${ICON_TYPE}.iconset/icon_${WIDTH}x${WIDTH}${ICON_DARK_SUFFIX}.png"
     }
 
     generate_images 512
@@ -39,12 +40,19 @@ convert_icon () {
     generate_images 16
 }
 
-convert_icon GodotLG-Default ../Export/GodotLG-macOS-Default-1024x1024@2x.webp
-convert_icon GodotLG-Dark ../Export/GodotLG-macOS-Dark-1024x1024@2x.webp
+mkdir -p GodotLG-Default.iconset/
+mkdir -p GodotLG-Dark.iconset/
 
-iconutil -c icns GodotLG-Default.iconset
-iconutil -c icns GodotLG-Dark.iconset
+convert_icon GodotLG-Default ../Export/GodotLG-macOS-Default-1024x1024@2x.webp ""
+convert_icon GodotLG-Dark ../Export/GodotLG-macOS-Dark-1024x1024@2x.webp "~dark"
 
-mv GodotLG-Default.icns ../Resources/
-mv GodotLG-Dark.icns ../Resources/
+mkdir -p GodotLG.iconset
+cp GodotLG-Default.iconset/* GodotLG.iconset/
+cp GodotLG-Dark.iconset/* GodotLG.iconset/
+rm -rf GodotLG-Default.iconset/
+rm -rf GodotLG-Dark.iconset/
+
+iconutil -c icns GodotLG.iconset
+
+mv GodotLG.icns ../Resources/
 
